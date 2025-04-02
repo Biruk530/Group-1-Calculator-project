a8fb3b37af25fd5a5c9a133c53c839de063fc7e3
yaredcomp <yaredderejeamu@gmail.com>
2025-04-02 13:25:50 +0300

    simplifide the code line for function defination

--- main.cpp
+++ main.cpp
@@ -32,19 +32,7 @@ int main() {
     return 0;
     }
     // Function definitions
-    int add(int a, int b) {
-    return a + b;
-    }
-    int subtract(int a, int b) {
-        return a - b;
-       }
-    int multiply(int a, int b) {
-        return a * b;
-        }
-    int divide(int a, int b) {
-        if (b == 0) {
-        cout << "Error: Division by zero!" << endl;
-        return 0;
-        }
-        return a / b;
-        }
+    int add(int a, int b) {return a + b;}
+    int subtract(int a, int b) {return a - b;}
+    int multiply(int a, int b) {return a * b;}
+    int divide(int a, int b) {if (b == 0) {cout << "Error: Division by zero!" << endl;return 0;}return a / b;}

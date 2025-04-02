a1edc7dd2493cfc8fb95f8f9bf3334f673f69b6d
dibora <diboratesfalem32@gimail.com>
2025-04-02 11:51:47 +0300

    added multiplication and division

--- main.cpp
+++ main.cpp
@@ -38,3 +38,13 @@ int main() {
     int subtract(int a, int b) {
         return a - b;
        }
+    int multiply(int a, int b) {
+        return a * b;
+        }
+    int divide(int a, int b) {
+        if (b == 0) {
+        cout << "Error: Division by zero!" << endl;
+        return 0;
+        }
+        return a / b;
+        }
